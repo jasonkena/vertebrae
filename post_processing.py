f896f3126f3ae8b6473da91b38a1f689234f8820
Jason Adhinarta <jasonice6@gmail.com>
2024-04-23 01:09:23 -0400

    mesh repairing occaxssionally wipes mesh

--- post_processing.py
+++ post_processing.py
@@ -5,9 +5,11 @@ import argparse
 import numpy as np
 import nibabel as nib
 
-from probreg import cpd
+from probreg import cpd, bcpd
 from tqdm import tqdm
 
+import pyvista as pv
+import pymeshfix as mf
 
 """
 reference was obtained from Totalsegmentator_dataset/s0224, a case which had complete vertebrae annotations
@@ -54,88 +56,103 @@ def read_reference(reference_path):
     return label
 
 
-def match(pc_ref, pc_pred, cpd_type, num_sample, match_sigma, use_cuda):
-    # num_sample required to avoid OOM
-    # center both point clouds to make it easier to match
-    pc_ref_mean = np.mean(pc_ref, axis=0)
-    pc_pred_mean = np.mean(pc_pred, axis=0)
-    pc_ref = pc_ref - pc_ref_mean
-    pc_pred = pc_pred - pc_pred_mean
+def align_mesh(ref_mesh, pred_mesh, cpd_type, target_reduction, cpd_kwargs):
+    # takes in ref_mesh and pred_mesh, returns aligned ref_mesh
+    # NOTE: modifies ref_mesh and pred_mesh in place
 
-    # cpd registration
-    # allow sampling with replacement just in case len(pc) < num_sample
-    pc_ref_sample = pc_ref[np.random.choice(pc_ref.shape[0], num_sample, replace=True)]
-    pc_pred_sample = pc_pred[
-        np.random.choice(pc_pred.shape[0], num_sample, replace=True)
-    ]
+    ref_mean = ref_mesh.points.mean(axis=0)
+    pred_mean = pred_mesh.points.mean(axis=0)
+    ref_std = ref_mesh.points.std(axis=0)
+    pred_std = pred_mesh.points.std(axis=0)
 
-    # add noise, to prevent regularly spaced points
-    pc_ref_sample += np.random.normal(0, match_sigma, pc_ref_sample.shape)
-    pc_pred_sample += np.random.normal(0, match_sigma, pc_pred_sample.shape)
+    # normalize to unit sphere
+    ref_mesh.points = (ref_mesh.points - ref_mean) / ref_std
+    pred_mesh.points = (pred_mesh.points - pred_mean) / pred_std
+
+    # decimate to reduce number of triangle vertices
+    ref_mesh = ref_mesh.decimate(target_reduction)
+    pred_mesh = pred_mesh.decimate(target_reduction)
+
+    # ensure watertightness; applying the transformation might cause overlapping triangles, and then mesh.repair() will delete all the faces, nuking the mesh
+    ref_mesh = fix_mesh(ref_mesh)
+    pred_mesh = fix_mesh(pred_mesh)
 
     tf_param, _, _ = cpd.registration_cpd(
-        pc_ref_sample, pc_pred_sample, tf_type_name=cpd_type, use_cuda=use_cuda
+        ref_mesh.points,
+        pred_mesh.points,
+        tf_type_name=cpd_type,
+        **cpd_kwargs,
     )
-    transformed = tf_param.transform(pc_ref) + pc_pred_mean
 
-    return transformed
+    aligned_ref_pc = tf_param.transform(ref_mesh.points) * pred_std + pred_mean
+    # tf_param = bcpd.registration_bcpd(ref_mesh.points, pred_mesh.points)
+    # aligned_ref_pc = tf_param.transform(ref_mesh.points) * pred_std + pred_mean
 
+    ref_mesh.points = aligned_ref_pc
 
-def densify(pc, num_duplicate, densify_sigma):
-    points = [pc]
-    for _ in range(num_duplicate):
-        points.append(pc + np.random.normal(0, densify_sigma, pc.shape))
-    return np.concatenate(points, axis=0)
+    return ref_mesh
 
 
-def voxelize(shape, pc):
+def fix_mesh(mesh, joincomp=False, remove_smallest_components=True):
+    mesh_fix = mf.MeshFix(mesh)
+    mesh_fix.repair(
+        joincomp=joincomp,
+        remove_smallest_components=remove_smallest_components,
+        verbose=True,
+    )
+    if mesh_fix.mesh.points.shape[0] < 200:
+        raise ValueError("MeshFix nuked mesh")
+    return mesh_fix.mesh
+
+
+def voxel_to_mesh(vol):
+    # https://docs.pyvista.org/version/stable/examples/00-load/create-uniform-grid.html
+    grid = pv.ImageData()
+    grid.dimensions = vol.shape
+    grid.point_data["values"] = vol.flatten(order="F")
+
+    # create mesh by running flying edges with isocontour value of 0.5
+    mesh = grid.contour([0.5])
+    return mesh
+
+
+def mesh_to_voxel(mesh, shape):
+    # crashes if check_surface is True, very weird
+    grid = pv.voxelize(mesh, density=1, check_surface=False)
+    # flooring should be fine, see https://github.com/pyvista/pyvista/blob/392d29ea4398c7292275300b44b75bcdbbed1c2e/pyvista/core/utilities/features.py#L78-L82
+    points = np.floor(grid.points).astype(int)
+    points = np.clip(points, 0, np.array(shape) - 1)
+
     result = np.zeros(shape, dtype=int)
-    pc = np.round(pc).astype(int)
-    pc = np.clip(pc, 0, np.array(shape) - 1)
-    result[pc[:, 0], pc[:, 1], pc[:, 2]] = 1
+    result[points[:, 0], points[:, 1], points[:, 2]] = 1
 
     return result
 
 
-def post_process(
-    reference,
-    prediction,
-    cpd_type,
-    num_sample,
-    match_sigma,
-    use_cuda,
-    num_duplicate,
-    densify_sigma,
-):
+def post_process(reference, prediction, cpd_type, target_reduction, cpd_kwargs):
     result = np.zeros(prediction.shape, dtype=int)
     for i in tqdm(range(1, len(ORDERING) + 1), leave=False):
-        ref_pc = np.array(np.where(reference == i)).T.astype(float)
-        pred_pc = np.array(np.where(prediction == i)).T.astype(float)
-
-        if len(pred_pc) == 0:
+        # if component missing, just skip it
+        if np.sum(prediction == i) == 0:
             continue
 
+        ref_mesh = voxel_to_mesh(reference == i)
+        pred_mesh = voxel_to_mesh(prediction == i)
+
         # deform reference to prediction
-        transformed_pc = match(
-            ref_pc, pred_pc, cpd_type, num_sample, match_sigma, use_cuda
+        transformed_mesh = align_mesh(
+            ref_mesh, pred_mesh, cpd_type, target_reduction, cpd_kwargs
         )
-        densified_pc = densify(transformed_pc, num_duplicate, densify_sigma)
-        segmentation = voxelize(prediction.shape, densified_pc)
+        # transformed_mesh = fix_mesh(transformed_mesh)
+
+        segmentation = mesh_to_voxel(transformed_mesh, prediction.shape)
 
         result = result * (segmentation == 0) + segmentation * i
     return result
 
 
 def main(
-    reference_path,
-    predict_path,
-    output_path,
-    cpd_type,
-    num_sample,
-    match_sigma,
-    use_cuda,
-    num_duplicate,
-    densify_sigma,
+    reference_path, predict_path, output_path, cpd_type, target_reduction, cpd_kwargs
 ):
     if not os.path.exists(output_path):
         os.makedirs(output_path)
@@ -148,11 +165,8 @@ def main(
             reference,
             prediction,
             cpd_type,
-            num_sample,
-            match_sigma,
-            use_cuda,
-            num_duplicate,
-            densify_sigma,
+            target_reduction,
+            cpd_kwargs,
         )
 
         name = os.path.basename(folder)
@@ -181,12 +195,14 @@ if __name__ == "__main__":
         type=str,
         default="/data/adhinart/zongwei/AbdomenAtlasDemoPredictPostProcessed",
     )
-    parser.add_argument("--num_sample", type=int, default=2048)
-    parser.add_argument("--match_sigma", type=int, default=1.0)
     parser.add_argument("--cpd_type", type=str, default="nonrigid")
     parser.add_argument("--use_cuda", action="store_true")  # requires cupy
-    parser.add_argument("--num_duplicate", type=int, default=4)
-    parser.add_argument("--densify_sigma", type=int, default=1)
+    # with 0.9, ~1000 points are used for matching
+    parser.add_argument(
+        "--target_reduction", type=int, default=0.9
+    )  # simplifying number of vertices in mesh
+
+    cpd_kwargs = {"lmd": 1e-3, "beta": 1e-3}
 
     args = parser.parse_args()
 
@@ -197,9 +213,6 @@ if __name__ == "__main__":
         args.predict_path,
         args.output_path,
         args.cpd_type,
-        args.num_sample,
-        args.match_sigma,
-        args.use_cuda,
-        args.num_duplicate,
-        args.densify_sigma,
+        args.target_reduction,
+        cpd_kwargs,
     )
